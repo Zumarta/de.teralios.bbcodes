f65b6e858715912abdec85544029bade884d72ae
Teralios <k.achterrath@gmail.com>
2014-04-07 11:55:07 +0200

    New syntax for pro and contra bbcode. New style for pro and contra bbcode.

--- de.teralios.tjs.bbcodes/files/lib/system/bbcode/ProContraBBCode.class.php
+++ de.teralios.tjs.bbcodes/files/lib/system/bbcode/ProContraBBCode.class.php
@@ -2,6 +2,10 @@
 // namespace
 namespace wcf\system\bbcode;
 
+// imports
+use wcf\system\WCF;
+use wcf\util\StringUtil;
+
 /**
  * Pro and Contra BBCode for the wcf2.0.
  *
@@ -11,50 +15,61 @@ namespace wcf\system\bbcode;
  * @package de.teralios.tjs.bbcodes
  */
 class ProContraBBCode extends AbstractBBCode {
+	const PATTERN = '#\[([+-\\\*])\]#';
+
 	/**
 	 * @see	\wcf\system\bbcode\IBBCode::getParsedTag()
 	 */
 	public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) {
-		$content = \wcf\util\StringUtil::trim($content);
-		$title = (isset($openingTag['attributes'][0])) ? $openingTag['attributes'][0] : \wcf\system\WCF::getLanguage()->get('wcf.bbcode.proContra');
-		
-		// initialise data array.
-		$points = array('contra' => array(), 'neutral' => array(), 'pro' => array());
-		
-		// split on \n and <br />
-		$elements = preg_split('#\[(+|-|*)\]#', $elements, -1,PREG_SPLIT_DELIM_CAPTURE);
-		if (count($elements) > 0) {
-			// @todo rework
-		}
-	
-		if ($parser->getOutputType() == 'text/html') {
-			\wcf\system\WCF::getTPL()->assign(array(
+		$title = (isset($openingTag['attributes'][0])) ? $openingTag['attributes'][0] : WCF::getLanguage()->get('wcf.bbcode.proContra');
+		if (preg_match(self::PATTERN, $content)) {
+			// split on [+] [-] or [*]
+			$elements = array();
+			$elements = preg_split('#\[([+-\\\*])\]#', $content, -1, PREG_SPLIT_DELIM_CAPTURE);
+			
+			// remove first element.
+			unset($elements[0]);
+			
+			// build pairs
+			$pairs = array();
+			if (!empty($elements)) {
+				$i = 1;
+				$count = count($elements);
+				while ($i < $count) {
+					$value = StringUtil::trim($elements[($i + 1)]);
+					if (!empty($value) && $value != '<br />') {
+						$pairs[] = array('sign' => $elements[$i], 'value' => $elements[($i + 1)]);
+					}
+					$i =  $i+2;
+				}
+			}
+			
+			// build list array
+			$points = array('pro' => array(), 'contra' => array(), 'neutral' => array());
+			foreach ($pairs as $pair) {
+				switch ($pair['sign']) {
+					case '+':
+						$points['pro'][] = $pair['value'];
+						break;
+					case '-':
+						$points['contra'][] = $pair['value'];
+						break;
+					case '*':
+						$points['neutral'][] = $pair['value'];
+						break;
+				}
+			}
+			WCF::getTPL()->assign(array(
 				'title' => $title,
-				'points' => $points		
+				'points' => $points
 			));
-			return \wcf\system\WCF::getTPL()->fetch('proContraBBCodeTag');
+			$return = WCF::getTPL()->fetch('proContraBBCodeTag');
 		}
-		else if ($parser->getOutputType() == 'text/simplified-html') {
-			$result = $title.":<br />\n";
-			if (!empty($points['pro'])) {
-				foreach ($points['pro'] AS $point) {
-					$result .= '+ '.$point."<br />\n";
-				}
-			}
-			
-			if (!empty($points['contra'])) {
-				foreach ($points['contra'] AS $point) {
-					$result .= '- '.$point."<br />\n";
-				}
-			}
-			
-			if (!empty($points['neutral'])) {
-				foreach ($points['neutral'] AS $point) {
-					$result .= '* '.$point."<br />\n";
-				}
-			}
-			
-			return $result;
+		else {
+			// no valid list
+			$return = $openingTag['source'].$content.$closingTag['source'];
 		}
+		
+		return $return;
 	}
 }
--- de.teralios.tjs.bbcodes/templates/proContraBBCodeTag.tpl
+++ de.teralios.tjs.bbcodes/templates/proContraBBCodeTag.tpl
@@ -1,33 +1,65 @@
 <!-- Pro and Contra BBCode is part of de.teralios.tjs.bbcodes (Teralios.de) -->
-<div class="tabularBox proContraBox">
+<div class="tabularBox proContraBBCode">
 	<div class="tabularBoxTitle"><header><h2>{$title}</h2></header></div>
-	<div class="proContraBoxContent">
-	{if $points['pro']|count}
-		<div class="proBox">
-			<ul>
-				{foreach from=$points['pro'] item=$point}
-					<li><span class="icon icon16 icon-plus-sign icon-pro"></span> {@$point}</li>
-				{/foreach}
-			</ul>
+	{if $points|empty}<div>{lang}wcf.bbcode.proContra.empty{/lang}</div>{/if}
+	{if $proContraStyle|isset && $proContraStyle == 'old'}
+		<div class="content styleOld">
+			{if $points['pro']|count}
+				<div class="pro">
+					<ul>
+						{foreach from=$points['pro'] item=$point}
+							<li><span class="icon icon16 icon-plus-sign icon-pro"></span> {@$point}</li>
+						{/foreach}
+					</ul>
+				</div>
+			{/if}
+			{if $points['contra']|count}
+				<div class="contra">
+					<ul>
+						{foreach from=$points['contra'] item=$point}
+							<li><span class="icon icon16 icon-minus-sign icon-contra"></span> {@$point}</li>
+						{/foreach}
+					</ul>
+				</div>
+			{/if}
+			{if $points['neutral']|count}
+				<div class="neutral">
+					<ul>
+						{foreach from=$points['neutral'] item=$point}
+							<li><span class="icon icon16 icon-minus-sign"></span> {@$point}</li>
+						{/foreach}
+					</ul>
+				</div>
+			{/if}
+		</div>
+	{else}
+		<div class="content styleNew">
+				<div class="proAndContra">
+					{if $points['pro']|count}
+						<ul class="pro">
+							{foreach from=$points['pro'] item=$point}
+								<li><span class="icon icon16 icon-plus-sign icon-pro"></span> {@$point}</li>
+							{/foreach}
+						</ul>
+					{/if}
+					{if $points['contra']|count}
+						<ul class="contra">
+							{foreach from=$points['contra'] item=$point}
+								<li><span class="icon icon16 icon-minus-sign icon-contra"></span> {@$point}</li>
+							{/foreach}
+						</ul>
+					{/if}
+					<span class="clearfix"></span>
+				</div>
+				<div class="neutral">
+				{if $points['neutral']|count}
+						<ul>
+							{foreach from=$points['neutral'] item=$point}
+								<li><span class="icon icon16 icon-minus-sign"></span> {@$point}</li>
+							{/foreach}
+						</ul>
+					{/if}
+				</div>
 		</div>
 	{/if}
-	{if $points['contra']|count}
-		<div class="contraBox">
-			<ul>
-				{foreach from=$points['contra'] item=$point}
-					<li><span class="icon icon16 icon-minus-sign icon-contra"></span> {@$point}</li>
-				{/foreach}
-			</ul>
-		</div>
-	{/if}
-	{if $points['neutral']|count}
-		<div class="neutralBox">
-			<ul>
-				{foreach from=$points['neutral'] item=$point}
-					<li><span class="icon icon16 icon-minus-sign"></span> {@$point}</li>
-				{/foreach}
-			</ul>
-		</div>
-	{/if}
-	</div>
 </div>
