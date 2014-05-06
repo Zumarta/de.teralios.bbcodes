2fe4873404bd2d59d72ef37a80267d4a48de5f40
Karsten Achterrath <k.achterrath@gmail.com>
2014-05-06 10:37:33 +0200

    Change back to special style sheet...

--- de.teralios.tjs.bbcodes/files/lib/system/bbcode/XAttachBBCode.class.php
+++ de.teralios.tjs.bbcodes/files/lib/system/bbcode/XAttachBBCode.class.php
@@ -32,7 +32,13 @@ class XAttachBBCode extends AttachmentBBCode {
 		// get attachment link from attachment bbcode.
 		$attachmentLink = parent::getParsedTag($openingTag, '', $closingTag, $parser);
 
-		if ($parser->getOutputType() == 'text/html') {	
+		if ($parser->getOutputType() == 'text/html') {
+			// new size
+			if (ATTACHMENT_THUMBNAIL_WIDTH != 280) {
+				$width = ATTACHMENT_THUMBNAIL_WIDTH + (str_replace('px', '', StyleHandler::getStyle()->getVariable('wcfGapTiny')));
+				WCF::getTPL()->assign('xAttachSize', $width);
+			}
+			
 			WCF::getTPL()->assign(array(
 				'attachmentLink' => $attachmentLink,
 				'float' => $float,
