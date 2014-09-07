0ccccd0089e67dbe87faa31e269bc4a1c7f4d2d2
Teralios <teralios@posteo.de>
2014-09-07 18:02:06 +0200

    add base class for defintion list bbcode.

---
+++ files/lib/system/bbcode/DefinitionListBBCode.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace wcf\system\bbcode;
+
+// imports
+use wcf\util\StringUtil;
+
+class DefinitionListBBCode extends AbstractBBCode {
+	/**
+     * @see \wcf\system\bbcode\IBBCode::getParsedTag()
+     */
+    public function getParsedTag(array $openingTag, $content, array $closingTag, \wcf\system\bbcode\BBCodeParser $parser) {
+        
+    }
+}
