44ba3d30dbf7ee338b8830522710f46d3962a80d
Teralios <teralios@posteo.de>
2015-01-03 12:20:06 +0100

    beginn with xattach bbcode.

--- files/lib/system/bbcode/XAttachBBCode.class.php
+++ files/lib/system/bbcode/XAttachBBCode.class.php
@@ -2,6 +2,9 @@
 // namespace
 namespace wcf\system\bbcode;
 
+// imports
+use wcf\system\WCF;
+
 class XAttachBBCode extends AttachmentBBCode {
 	protected static $icons = array(
 		'default' => 'fa-file-o',
@@ -11,12 +14,19 @@ class XAttachBBCode extends AttachmentBBCode {
 		'pdf' => 'fa-file-pdf-o',
 		'video' => 'fa-file-video-o',
 		'audio' => 'fa-file-audio-o'
-	');
+	);
 	
 	/**
 	 * @see \wcf\system\bbcode\IBBCode::getParsedTag()
 	 */
 	public function getParsedTag(array $openingTag, $content, array $closingTag, \wcf\system\bbcode\BBCodeParser $parser) {
+		// call copyright
+		TeraliosBBCodesCopyright::callCopyright();
+		
+		// default values
 		$attachmentID = 0;
+		$float = 'none';
+		$title = WCF::getLanguage()->get('wcf.bbcode.xattach.title');
+		$type = 'default';
 	}
 }
