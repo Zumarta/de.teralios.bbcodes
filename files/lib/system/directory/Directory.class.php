8111e2bbef158a1bfe16ce33d0a5301d31c8097c
Teralios <teralios@posteo.de>
2015-01-17 18:38:35 +0100

    Add missing file

---
+++ files/lib/system/directory/Directory.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace wcf\system\directory;
+
+// imports
+use wcf\system\directory\entry\EntryNodeMap;
+
+/**
+ * Directory class
+ *
+ * @author	Karsten (Teralios) Achterrath
+ * @copyright	2014 Teralios.de
+ * @license	Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) <http://creativecommons.org/licenses/by-sa/4.0/legalcode>
+ * @package de.teralios.bbcodes
+ */
+class Directory extends EntryNodeMap {
+	
+}
