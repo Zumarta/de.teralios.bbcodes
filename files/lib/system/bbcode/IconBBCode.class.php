5b13c7cc4ffd9e4230c33c9634a2f59dc98d4d98
Teralios <teralios@posteo.de>
2015-01-25 17:48:38 +0100

    Remove error in icon bbcode class. Update RC 2

--- files/lib/system/bbcode/IconBBCode.class.php
+++ files/lib/system/bbcode/IconBBCode.class.php
@@ -9,9 +9,9 @@ use wcf\util\StringUtil;
 class IconBBCode extends AbstractBBCode {
 	public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) {
 		$icon = (isset($openingTag['attributes'][0])) ? StringUtil::trim($openingTag['attributes'][0]) : 'fa-rebel'; // Yes, Rebel icon as default icon! ;)
-		$float = (isset($openingTag['attributes'][0])) ? StringUtil::trim($openingTag['attributes'][1]) : 'none';
+		$float = (isset($openingTag['attributes'][1])) ? StringUtil::trim($openingTag['attributes'][1]) : 'none';
 		$size = (isset($openingTag['attributes'][2])) ? $openingTag['attributes'][2] : 16;
 		
-		$return = '<span class="fa icon'.$size.' '.$icon.'" '.(($float != 'none') ? 'style="float: '.$float.';"' : '').'></span>';
+		return '<span class="fa icon'.$size.' '.$icon.'" '.(($float != 'none') ? 'style="float: '.$float.';"' : '').'></span>';
 	}
 }
