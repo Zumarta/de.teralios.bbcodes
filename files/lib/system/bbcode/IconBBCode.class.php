3418a526ce0bdbf188f753c3183c4b74af580bfb
Teralios <teralios@posteo.de>
2015-01-26 18:10:13 +0100

    implement copyright for icon bbcode.

--- files/lib/system/bbcode/IconBBCode.class.php
+++ files/lib/system/bbcode/IconBBCode.class.php
@@ -3,10 +3,22 @@
 namespace wcf\system\bbcode;
 
 // imports
+use wcf\system\copyright\TeraliosBBCodesCopyright;
 use wcf\util\StringUtil;
 
+/**
+ * Icon bbcode.
+ *
+ * @author	Karsten (Teralios) Achterrath
+ * @copyright	2014 Teralios.de
+ * @license	Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) <http://creativecommons.org/licenses/by-sa/4.0/legalcode>
+ * @package de.teralios.bbcodes
+ */
 class IconBBCode extends AbstractBBCode {
 	public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) {
+		// copyright
+		TeraliosBBCodesCopyright::callCopyright();
+		
 		$icon = (isset($openingTag['attributes'][0])) ? StringUtil::trim($openingTag['attributes'][0]) : 'fa-rebel'; // Yes, Rebel icon as default icon! ;)
 		$float = (isset($openingTag['attributes'][1])) ? StringUtil::trim($openingTag['attributes'][1]) : 'none';
 		$size = (isset($openingTag['attributes'][2])) ? $openingTag['attributes'][2] : 16;
