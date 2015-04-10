faf3a7feb0b6f78fbb1e49963cc9bc52746dff1f
teralios <teralios@posteo.de>
2015-04-10 20:45:10 +0200

    update date and remove little errors.

--- files/lib/system/directory/entry/Entry.class.php
+++ files/lib/system/directory/entry/Entry.class.php
@@ -130,7 +130,7 @@ class Entry {
 		else if (URL_LEGACY_MODE) {
 			return WCF::getRequestURI();
 		}
-		else if (!strstr(WCF::getRequestURI(), 'index.php')) {
+		else if (!stristr(WCF::getRequestURI(), 'index.php')) {
 			return str_replace(WCF::getTPL()->get('baseHref'), WCF::getTPL()->get('baseHref').'index.php', WCF::getRequestURI());
 		}
 		else {
