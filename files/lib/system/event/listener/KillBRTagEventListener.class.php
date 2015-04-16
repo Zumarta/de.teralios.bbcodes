abd913b631f6bb110f3a0df03269cecf42795637
teralios <teralios@posteo.de>
2015-04-16 17:00:17 +0200

    Remove <br /> after block html tags!

--- files/lib/system/event/listener/KillBRTagEventListener.class.php
+++ files/lib/system/event/listener/KillBRTagEventListener.class.php
@@ -6,8 +6,7 @@ use wcf\system\event\IEventListener;
 class KillBRTagEventListener implements IEventListener {
 	public function execute($eventObj, $className, $eventName) {
 		$message = $eventObj->message;
-		
-		$message = preg_match('#(<\/(h1|h2|div|dl)>)[\s]*<br( \/)?>#i', '$1', $message);
+		$message = preg_replace('#(<\/(h1|h2|div|dl)>)[\s]*<br( \/)?>#i', '\\1', $message);
 		$eventObj->message = $message;
 	}
 }
