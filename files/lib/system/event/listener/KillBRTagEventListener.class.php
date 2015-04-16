e71e6dcbc6d7ab92e80a29a7e374d88fdafd3b9e
teralios <teralios@posteo.de>
2015-04-16 15:37:43 +0200

    remove <br /> after block elements.

---
+++ files/lib/system/event/listener/KillBRTagEventListener.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace wcf\system\event\listener;
+
+use wcf\system\event\IEventListener;
+
+class KillBRTagEventListener implements IEventListener {
+	public function execute($eventObj, $className, $eventName) {
+		$message = $eventObj->message;
+		
+		$message = preg_match('#(<\/(h1|h2|div|dl)>)[\s]*<br( \/)?>#i', '$1', $message);
+		$eventObj->message = $message;
+	}
+}
